f7a526f32e29b5cb3395712f6a1296f9a2ef929e
DanielMinarik11 <minarikdaniel5@gmail.com>
2026-02-12 23:31:40 +0100

    commit2

--- smartjob/src/app/home/home.ts
+++ smartjob/src/app/home/home.ts
@@ -11,19 +11,58 @@ interface Job {
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [], // CommonModule už v Angular 21 často netreba pri novom flow
+  imports: [], 
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-  // Simulované dáta pomocou signálov
+ 
   isLoggedIn = signal(true);
   
   featuredJobs = signal<Job[]>([
-    { id: 1, title: 'Senior Angular Developer', company: 'TechSolutions', location: 'Bratislava / Remote', salary: '3500€ - 5000€' },
-    { id: 2, title: 'Frontend Lead', company: 'Innovate AI', location: 'Košice', salary: '4000€+' },
-    { id: 3, title: 'Fullstack Dev (Node/Angular)', company: 'WebFlow', location: 'Remote', salary: '3000€ - 4500€' }
-  ]);
+  { 
+    id: 1, 
+    title: 'Predavač / Pokladník', 
+    company: 'Lidl Slovenská republika', 
+    location: 'Bratislava - Ružinov', 
+    salary: '1100€ - 1350€' 
+  },
+  { 
+    id: 2, 
+    title: 'Čašník / Servírka', 
+    company: 'Reštaurácia Staré Mesto', 
+    location: 'Košice / Centrum', 
+    salary: '900€ + tringelty' 
+  },
+  { 
+    id: 3, 
+    title: 'Samostatný kuchár', 
+    company: 'Hotel Riverside', 
+    location: 'Poprad / Vysoké Tatry', 
+    salary: '1500€ - 2000€' 
+  },
+  { 
+    id: 4, 
+    title: 'Skladník / Vodič ', 
+    company: 'Logistics Park', 
+    location: 'Senec', 
+    salary: '1200€ - 1450€' 
+  },
+  { 
+    id: 5, 
+    title: 'Barman', 
+    company: 'Sky Bar & Lounge', 
+    location: 'Bratislava', 
+    salary: '1000€ + bonusy' 
+  },
+  { 
+    id: 6, 
+    title: 'Upratovačka / Chyžná', 
+    company: 'Penzión Pohoda', 
+    location: 'Banská Bystrica', 
+    salary: '850€ - 950€' 
+  }
+]);
 
   constructor() {}
 }
